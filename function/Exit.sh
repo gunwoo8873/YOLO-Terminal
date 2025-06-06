1f581093c0a7fd9b8a0072da422565a8d9db53e4
choigunwoo <gunwoo8873@outlook.kr>
2025-06-06 15:04:36 +0900

    Update the add Git configure checking and read feature

---
+++ function/Exit.sh
@@ -0,0 +1,3 @@
+# !/bin/env bash
+
+read -n 1 -s -r -p "Press any key to continue.."
---
+++ git.sh
@@ -0,0 +1,23 @@
+# !/bin/env bash
+
+GitConfigPath="C:/Users/gunwo/.gitconfig"
+ExitPath="./function/Exit.sh"
+
+function ReadFile() {
+  if [[ -f $GitConfigPath ]]; then
+    echo "Checking Complete have git config file"
+
+  elif [[ -e $GitConfigPath ]]; then
+    echo "Not found git config file"
+    read -p "You want create giconfig file [Y/N]?" $SELECTE_INPUT
+
+    if [[ $SELECTE_INPUT == [yY] ]]; then
+      echo "Creating git config file, wait please"
+
+    elif [[ $SELECTE_INPUT == [nN] ]] && [[ -r $ExitPath ]]; then
+      bash $ExitPath
+    fi
+  fi
+}
+
+ReadFile
