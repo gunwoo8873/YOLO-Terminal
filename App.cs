afd262c7d8062374eb5c7d8e025f27031c572bca
choigunwoo <gunwoo8873@outlook.kr>
2025-06-06 21:28:50 +0900

    Update Add button to file, options, exit

--- App.cs
+++ App.cs
@@ -1,28 +1,57 @@
 ﻿using Terminal.Gui;
 
-namespace Terminal
+namespace Gui
 {
-  public class RunTime
+  public class Layout
   {
     public static void Run()
     {
       // Init runtime
       Application.Init();
-      
+
       // Return to window title and layout
-      var win = Element.Title();
-      Application.Top.Add(win);
+      var win = View.Text.Title();
+      var menu = View.Button.Menu();
+      Application.Top.Add(win, menu);
 
       // UI load runtime
       Application.Run();
     }
   }
 
-  public class Element
+  // public class Element
+  // {
+  //   public static Window Title()
+  //   {
+  //     // var top = Application.Top;
+  //     // Window size and main title feature
+  //     var win = new Window("Test Terminal.Gui")
+  //     {
+  //       X = 0,
+  //       Y = 0,
+  //       Width = Dim.Fill(),
+  //       Height = Dim.Fill(),
+  //     };
+
+  //     // Feature layout
+  //     var label = new Label("Test Label")
+  //     {
+  //       X = Pos.Center(),
+  //       Y = Pos.Center()
+  //     };
+  //     win.Add(label);
+
+  //     return win;
+  //   }
+  // }
+}
+
+namespace View
+{
+  public class Text
   {
     public static Window Title()
     {
-      // var top = Application.Top;
       // Window size and main title feature
       var win = new Window("Test Terminal.Gui")
       {
@@ -32,23 +61,46 @@ namespace Terminal
         Height = Dim.Fill(),
       };
 
-      // Feature layout
-      var label = new Label("Test Label")
-      {
-        X = Pos.Center(),
-        Y = Pos.Center()
-      };
-      win.Add(label);
+      // var label = new Label("Test Label")
+      // {
+      //   X = Pos.Center(),
+      //   Y = Pos.Center()
+      // };
+      // win.Add(label);
 
       return win;
     }
   }
+
+  public class Button
+  {
+    public static MenuBar Menu()
+    {
+      var menu = new MenuBar(new MenuBarItem[] {
+        new MenuBarItem("File", new MenuItem[] {
+          // @gunwoo8873 : Add, Remove, Move, etc... and create date timestamp checking for looking
+          // new MenuItem("Add", "",)
+          // new MenuItem("Remove", "",)
+          // new MenuItem("Move", "",)
+        }),
+        new MenuBarItem("Options", new MenuItem[] {
+          // @gunwoo8873 : Options action to color for background, font, etc... selected
+          // new MenuItem("Color", "", () => Application.Add())
+        }),
+        // @gunwoo8873 : Exit action to new small pop window and Y/N selected
+        new MenuBarItem("_Exit", "", () => Application.RequestStop()),
+      });
+
+      return menu;
+    }
+  }
 }
 
-public class Compiler
+
+public class RunTime
 {
   public static void Main(string[] args)
   {
-    Terminal.RunTime.Run();
+    Gui.Layout.Run();
   }
 }
