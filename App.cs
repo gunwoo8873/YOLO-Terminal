c723371c253fde65645dbf8fe0cfe138f1aa3e48
choigunwoo <gunwoo8873@outlook.kr>
2025-07-20 17:27:22 +0900

    Fix

--- App.cs
+++ App.cs
@@ -1,6 +1,6 @@
 ﻿using Terminal.Gui;
 
-namespace Gui
+namespace Screen
 {
   public class Layout
   {
@@ -10,45 +10,19 @@ namespace Gui
       Application.Init();
 
       // Return to window title and layout
-      var win = View.Text.Title();
-      var menu = View.Button.Menu();
+      var win = Headers.Title.Title();
+      var menu = Headers.Button.Menu();
       Application.Top.Add(win, menu);
 
       // UI load runtime
       Application.Run();
     }
   }
-
-  // public class Element
-  // {
-  //   public static Window Title()
-  //   {
-  //     // var top = Application.Top;
-  //     // Window size and main title feature
-  //     var win = new Window("Test Terminal.Gui")
-  //     {
-  //       X = 0,
-  //       Y = 0,
-  //       Width = Dim.Fill(),
-  //       Height = Dim.Fill(),
-  //     };
-
-  //     // Feature layout
-  //     var label = new Label("Test Label")
-  //     {
-  //       X = Pos.Center(),
-  //       Y = Pos.Center()
-  //     };
-  //     win.Add(label);
-
-  //     return win;
-  //   }
-  // }
 }
 
-namespace View
+namespace Headers
 {
-  public class Text
+  public class Title
   {
     public static Window Title()
     {
@@ -78,17 +52,14 @@ namespace View
     {
       var menu = new MenuBar(new MenuBarItem[] {
         new MenuBarItem("File", new MenuItem[] {
-          // @gunwoo8873 : Add, Remove, Move, etc... and create date timestamp checking for looking
-          // new MenuItem("Add", "",)
-          // new MenuItem("Remove", "",)
-          // new MenuItem("Move", "",)
+          new MenuItem("Add", "", () => {}),
+          new MenuItem("Remove", "", () => {}),
+          new MenuItem("Move", "", () => {}),
         }),
         new MenuBarItem("Options", new MenuItem[] {
-          // @gunwoo8873 : Options action to color for background, font, etc... selected
-          // new MenuItem("Color", "", () => Application.Add())
+          new MenuItem("Color", "", () => {}),
         }),
-        // @gunwoo8873 : Exit action to new small pop window and Y/N selected
-        new MenuBarItem("_Exit", "", () => Application.RequestStop()),
+        new MenuBarItem("Exit", "", () => Application.RequestStop()),
       });
 
       return menu;
@@ -96,11 +67,10 @@ namespace View
   }
 }
 
-
 public class RunTime
 {
   public static void Main(string[] args)
   {
-    Gui.Layout.Run();
+    Screen.Layout.Run();
   }
 }
